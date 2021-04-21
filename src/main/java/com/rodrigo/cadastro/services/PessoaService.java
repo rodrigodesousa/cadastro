3c7fab11fc7e0959c3b8afbec8e298e90029e073
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-04-21 13:56:28 -0400

    Resource e Service para endpoint /pessoa/{id}

--- src/main/java/com/rodrigo/cadastro/services/PessoaService.java
+++ src/main/java/com/rodrigo/cadastro/services/PessoaService.java
@@ -1,5 +1,21 @@
 package com.rodrigo.cadastro.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.cadastro.domain.Pessoa;
+import com.rodrigo.cadastro.repositories.PessoaRepository;
+
+@Service
 public class PessoaService {
 
+	@Autowired
+	private PessoaRepository repo;
+	
+	public Pessoa find(Integer id) {
+		Optional<Pessoa> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
 }
