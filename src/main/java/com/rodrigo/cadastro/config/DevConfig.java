741153628ef1e17783cf9ea844c3e8043de538ac
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-04-21 22:34:55 -0400

    Perfil dev

---
+++ src/main/java/com/rodrigo/cadastro/config/DevConfig.java
@@ -0,0 +1,24 @@
+package com.rodrigo.cadastro.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rodrigo.cadastro.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbservice;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbservice.instantiateTestDatabase();
+		return true;
+	}
+}
